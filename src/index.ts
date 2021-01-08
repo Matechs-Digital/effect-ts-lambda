b2275e2e04b27448fc3e9cca05bb3bf11071884a
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-08 19:58:38 +0000

    improve

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,20 @@
-import "./utils/init";
+import "./utils/Init";
 
 import * as T from "@effect-ts/core/Effect";
 import { pipe } from "@effect-ts/core/Function";
 import * as R from "@effect-ts/node/Runtime";
-import { LiveAuth, login } from "./program/Auth";
-import { lambda } from "./program/Lambda";
-
-export type Event = { _tag: "fail" } | { _tag: "login"; token: string };
-
-export const { handler, main } = lambda((event: Event) =>
-  pipe(
-    login,
-    T.chain((token) =>
-      T.effectTotal(() => {
-        if (event._tag === "fail") {
-          throw new Error("simulate defect exception");
-        }
-        if (event.token !== token) {
-          return {
-            message: "not authorized",
-          };
-        }
-        return {
-          response: "success",
-        };
-      })
-    )
-  )
-);
+import { LiveAuth } from "./utils/Auth";
+import { handler, main } from "./program";
 
 /**
  * As soon as the file loads we run the main process
  */
 pipe(
   main,
-  // we use the LiveAuth and the GlobalRequestQueue layer passing the global queue
+  // we use the LiveAuth
   T.provideSomeLayer(LiveAuth),
   // we run the main process, this will listen to process.on(exit) and interrupt correctly
   R.runMain
 );
+
+export { handler };
--- src/program/index.ts
+++ src/program/index.ts
@@ -1,2 +1,27 @@
-export { lambda } from "./Lambda";
-export { Auth, login, LiveAuth } from "./Auth";
+import * as T from "@effect-ts/core/Effect";
+import { pipe } from "@effect-ts/core/Function";
+import { login } from "../utils/Auth";
+import { lambda } from "../utils/Lambda";
+
+export type Event = { _tag: "fail" } | { _tag: "login"; token: string };
+
+export const { handler, main } = lambda((event: Event) =>
+  pipe(
+    login,
+    T.chain((token) =>
+      T.effectTotal(() => {
+        if (event._tag === "fail") {
+          throw new Error("simulate defect exception");
+        }
+        if (event.token !== token) {
+          return {
+            message: "not authorized",
+          };
+        }
+        return {
+          response: "success",
+        };
+      })
+    )
+  )
+);
