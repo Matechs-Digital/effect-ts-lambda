119e0d243595f1c8d988fc33e07c46bfa818c37a
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-08 19:54:29 +0000

    remove middle layer

--- src/program/Lambda.ts
+++ src/program/Lambda.ts
@@ -21,46 +21,9 @@ export function lambda<R, I, O>(f: (a: I) => T.RIO<R, O>) {
   };
 
   /**
-   * Environment Service to hold the Request Queue
-   */
-  interface RequestQueue<I, O> {
-    readonly _tag: "@app/RequestQueue";
-    readonly requestQueue: Q.Queue<RequestContext<I, O>>;
-  }
-
-  /**
    * Given the lambda handler is global we need to store the queue in a global variable
    */
   const requestQueue = Q.unsafeMakeUnbounded<RequestContext<I, O>>();
-
-  /**
-   * Tag to access and provide the RequestQueue
-   */
-  const RequestQueue = tag<RequestQueue<I, O>>();
-
-  /**
-   * this is supposed to be called with an already constructed queue that will be closed
-   * in case the process is either interrupted or ends for any reason
-   */
-  const GlobalRequestQueue = L.fromManaged(RequestQueue)(
-    pipe(
-      T.succeed({ _tag: literal("@app/RequestQueue"), requestQueue }),
-      M.makeExit(({ requestQueue }) =>
-        pipe(
-          requestQueue.takeAll,
-          T.tap(() => requestQueue.shutdown),
-          T.chain(T.foreach(({ res }) => T.to(res)(T.interrupt)))
-        )
-      )
-    )
-  );
-
-  /**
-   * Access takeRequest from environment and use it
-   */
-  const takeRequest = T.accessServiceM(RequestQueue)(
-    ({ requestQueue }) => requestQueue.take
-  );
 
   /**
    * Returns an effect that will poll indefinately the request queue
@@ -69,7 +32,7 @@ export function lambda<R, I, O>(f: (a: I) => T.RIO<R, O>) {
   const main = T.accessM((env: R) =>
     pipe(
       // poll from the request queue waiting in case no requests are present
-      takeRequest,
+      requestQueue.take,
       // fork each request in it's own fiber and start processing
       // here we are forking inside the parent scope so in case the
       // parent is interrupted each of the child will also trigger
@@ -77,8 +40,14 @@ export function lambda<R, I, O>(f: (a: I) => T.RIO<R, O>) {
       T.chain((r) => T.fork(P.complete(T.provideAll(env)(f(r.event)))(r.res))),
       // loop forever
       T.forever,
-      // wrap layer to handle teardown
-      T.provideSomeLayer(GlobalRequestQueue)
+      // handle teardown
+      T.ensuring(
+        pipe(
+          requestQueue.takeAll,
+          T.tap(() => requestQueue.shutdown),
+          T.chain(T.foreach(({ res }) => T.to(res)(T.interrupt)))
+        )
+      )
     )
   );
 
